eb3766bf3797c632a1ffafefab6d6e30171981c1
Francis3320 <darkofrancis18@yahoo.com>
2023-05-31 03:20:00 +0100

    header and navbar:done

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './header.css'
+import me from '../../assets/me.jpg'
+import HeaderSocials  from './HeaderSocials'
+
+
+const Header = () => {
+  return (
+    <header>
+      <div className='container header_container'>
+        <h5>Hello I'm</h5>
+        <h1>Darko Francis</h1>
+        <h5 className='text-light'>Frontend Developer</h5>
+          <div className='cta'>
+          <a href='' className='btn'>Download CV</a>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+          </div>
+          <a href='#contact' className='scroll_down'>Scroll down</a>
+          <HeaderSocials />
+          <div className='me'>
+            <img src={me} alt='me' />
+          </div>
+          
+      </div>
+
+    </header>
+  )
+}
+
+export default Header;
