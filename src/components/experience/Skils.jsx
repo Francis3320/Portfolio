ec8c258d3a6384f33a53ceade552185ad4143ec2
Francis3320 <darkofrancis18@yahoo.com>
2023-06-13 03:45:34 +0100

    chore:skils:services

--- src/components/experience/Skils.jsx
+++ src/components/experience/Skils.jsx
@@ -1,11 +1,68 @@
 import React from 'react'
 import './skills.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Skills = () => {
   return (
+    <div className='skills_container'>
     <section id='skills'>
+    <h5>What Skills I Have</h5>
+    <h2>My Experience</h2>
 
+    <div className='container experience_container'>
+    <div className='experience_frontend'>
+        <h3>Skills</h3>
+        <div className='experience_content'>
+          <article className='experience_details'>
+            <BsPatchCheckFill className='icons'/>
+            <div>
+            <h4>HTML</h4>
+            <small className='text-light'>Experienced</small>
+            </div>
+          </article>
+          <article className='experience_details'>
+            <BsPatchCheckFill className='icons' />
+           <div>
+           <h4>CSS</h4>
+            <small className='text-light'>Intermediate</small>
+           </div>
+          </article>
+          <article className='experience_details'>
+            <BsPatchCheckFill className='icons' />
+           <div>
+           <h4>JavaScript</h4>
+            <small className='text-light'>Intermediate</small>
+           </div>
+          </article>
+          <article className='experience_details'>
+            <BsPatchCheckFill className='icons' />
+           <div>
+           <h4>React</h4>
+            <small className='text-light'>Intermediate</small>
+           </div>
+          </article>
+          <article className='experience_details'>
+            <BsPatchCheckFill className='icons' />
+            <div>
+            <h4>Boostrap</h4>
+            <small className='text-light'>Intermediate</small>
+            </div>
+          </article>
+          <article className='experience_details'>
+            <BsPatchCheckFill className='icons'/>
+            <div>
+            <h4>Graphic Design</h4>
+            <small className='text-light'>Experienced</small>
+            </div>
+          </article>
+
+        </div>
+    </div>
+
+    </div>
     </section>
+    </div>
+    
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,12 +1,85 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 
 
 const Services = () => {
   return (
    <section id='services'>
-
+      <h5>What I do</h5>
+      <h2>Services</h2>
+      <div className='container services_container'>
+        <article className='services'>
+          <div className='services_head'>
+                <h3>UI/UX Design</h3>
+          </div>
+          <ul className='service_list'>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className='services'>
+          <div className='services_head'>
+                <h3>Web Design</h3>
+          </div>
+          <ul className='service_list'>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className='services'>
+          <div className='services_head'>
+                <h3> Graphic Design </h3>
+          </div>
+          <ul className='service_list'>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className = 'icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
    </section>
   )
 }
